cafe7a16badf64561fd294492ef3af108443a43b
Maxime Tricoire <leiko@braindead.fr>
2013-04-26 12:29:50 +0200

    name change

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 
 exports.index = function(req, res) {
-    res.render('index', { title: 'KevWebEditor' });
+    res.render('index', { title: 'Kevoree Web Editor' });
 };
 
 exports.merge = require('./merge').merge;
