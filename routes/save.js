c00e443041a8ccd84b0d25240cb3309b1a1a540d
Maxime Tricoire <leiko@braindead.fr>
2013-04-30 17:37:58 +0200

    undownloaded saved model are also deleted after a period of time

--- routes/save.js
+++ routes/save.js
@@ -3,15 +3,17 @@
  */
 
 var fs = require('fs');
+var timeouts = [];
 
 exports.save = function(req, res) {
     if (req.xhr) {
         if (req.body.model) {
             var data = JSON.stringify(req.body.model, null, 4),
-                filename = '/saved/model'+Math.floor((Math.random()*42424242)+1)+'.json';
+                filename = '/saved/model'+Math.floor((Math.random()*42424242)+1)+'.json',
+                fullpath = 'public' + filename;
 
             fs.writeFile(
-                'public'+ filename, data, function(err) {
+                fullpath, data, function(err) {
                     if (err) {
                         console.warn(err);
                         res.json({
@@ -20,12 +22,29 @@ exports.save = function(req, res) {
                         });
 
                     } else {
-                        console.log("JSON saved to " + 'public' + filename);
+                        console.log("JSON saved to " + fullpath);
                         res.json({
                             state: 1,
-                            message: 'Your model has been uploaded to server successfully.',
+                            message: 'Your model has been successfully uploaded to the server.',
                             href: filename
                         });
+
+                        // delete this file in 30 minutes if it hasn't been accessed
+                        if (!timeouts[fullpath]) { timeouts[fullpath] = []; }
+                        var timeoutID = setTimeout(function () {
+                            try {
+                                fs.unlink(fullpath);
+                                // in case the file was asked many times, clear other timeouts
+                                timeouts[fullpath].forEach(function (id) {
+                                    clearTimeout(id);
+                                });
+                                // clear this file timeouts
+                                delete timeouts[filename];
+                            } catch (err) {
+                                console.warn("err "+err.message);
+                            }
+                        }, 1000*60*30);
+                        timeouts[fullpath].push(timeoutID);
                     }
             });
         }
@@ -34,4 +53,40 @@ exports.save = function(req, res) {
         // if this is not an Ajax request, then redirect to index
         res.redirect('/');
     }
+};
+
+/*
+ * GET json model
+ */
+
+exports.saved = function(req, res) {
+    var filename = 'public/saved/'+req.params.model;
+
+    fs.stat(filename, function(err, stats) {
+        if(!err && stats.isFile()) {
+            // send file back
+            res.download(filename);
+
+            // delete this file after 5 minutes
+            if (!timeouts[filename]) { timeouts[filename] = []; }
+            var timeoutID = setTimeout(function () {
+                try {
+                    fs.unlink(filename);
+                    // in case the file was asked many times, clear other timeouts
+                    timeouts[filename].forEach(function (id) {
+                        clearTimeout(id);
+                    });
+                    // clear this file timeouts
+                    delete timeouts[filename];
+                } catch (err) {
+                    console.warn("err "+err.message);
+                }
+            }, 1000*60*5);
+            timeouts[filename].push(timeoutID);
+
+        } else {
+            // file does not exist anymore
+            res.redirect('/');
+        }
+    });
 };
