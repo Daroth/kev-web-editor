e8fa27ab9be9379858aeadf8fa69e28a46464b65
Maxime Tricoire <leiko@braindead.fr>
2013-05-27 17:53:59 +0200

    css/js tweaks: responsiveness improved

--- public/js/app/runtime/ctrl/Runtime.js
+++ public/js/app/runtime/ctrl/Runtime.js
@@ -9,11 +9,14 @@ define(
             this._ui = new RuntimeUI(this);
         }
 
-        RuntimeController.prototype.p2cStartNode = function () {
-            this._ui.c2pNodeStarted();
+        RuntimeController.prototype.p2cStartNode = function (nodeName) {
+            // TODO
+            nodeName = (nodeName && nodeName.length > 0) ? nodeName : 'node0';
+            this._ui.c2pNodeStarted(nodeName);
         }
 
         RuntimeController.prototype.p2cStopNode = function () {
+            // TODO
             this._ui.c2pNodeStopped();
         }
 
--- public/js/app/runtime/ui/RuntimeUI.js
+++ public/js/app/runtime/ui/RuntimeUI.js
@@ -9,16 +9,44 @@ define(
         function RuntimeUI(ctrl) {
             this._ctrl = ctrl;
             this._tabCounter = $('#tabs-content .tab-pane').size();
+            this._header = $('#header');
+            this._tabsContainer = $('#tabs-container');
+            this._tabs = $('#tabs');
+            this._tabsPane = $('#tabs-container .tab-pane');
             this._startNodeBtn = $('#node-start');
             this._stopNodeBtn = $('#node-stop');
+            this._nodeName = $('#node-name');
 
+            configUI(this);
             registerCallbacks(this);
         }
 
         // private method
+        function configUI(ui) {
+            function resize() {
+                var fullHeight = $('body').outerHeight(true),
+                    headerHeight = ui._header.outerHeight(true),
+                    tabsContainerPaddingTop = ui._tabsContainer.css('padding-top'),
+                    tabsHeight = ui._tabs.outerHeight(true);
+
+                var computedPadding = parseInt(tabsContainerPaddingTop.substr(0, tabsContainerPaddingTop.length-2));
+                tabsContainerPaddingTop = (computedPadding == 0) ? 20 : computedPadding;
+                ui._tabsPane.height((fullHeight - headerHeight - tabsHeight - tabsContainerPaddingTop)+'px');
+            }
+            $(document).ready(resize);
+            $(window).resize(resize);
+        }
+
+        // private method
         function registerCallbacks(ui) {
+            ui._nodeName.on('keyup', function (e) {
+                if(e.keyCode == 13) {
+                    ui._ctrl.p2cStartNode(ui._nodeName.val());
+                }
+            });
+
             ui._startNodeBtn.on('click', function () {
-                ui._ctrl.p2cStartNode();
+                ui._ctrl.p2cStartNode(ui._nodeName.val());
             });
 
             ui._stopNodeBtn.on('click', function () {
@@ -42,7 +70,8 @@ define(
             $("a[href='#"+tabID+"']").effect('highlight', {color: '#fff'}, 500);
         }
 
-        RuntimeUI.prototype.c2pNodeStarted = function () {
+        RuntimeUI.prototype.c2pNodeStarted = function (nodeName) {
+            this._nodeName.val(nodeName);
             this._startNodeBtn.addClass('disabled');
             this._stopNodeBtn.removeClass('disabled');
         }
