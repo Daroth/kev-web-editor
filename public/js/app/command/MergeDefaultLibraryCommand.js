8b597c47d4479221921ca63932e747b162e49289
Maxime Tricoire <leiko@braindead.fr>
2013-06-03 14:03:12 +0200

    removing new code for load (still fixing it)

--- public/js/app/command/MergeDefaultLibraryCommand.js
+++ public/js/app/command/MergeDefaultLibraryCommand.js
@@ -16,29 +16,29 @@ define(
             AlertPopupHelper.show();
 
             $.getJSON('/merge/'+lib, function (model) {
-//                try {
+                try {
                     // TODO allow merge, this is not a merge, it replaces the old model if there is one
-                    var loader = new Kevoree.org.kevoree.loader.JSONModelLoader();
-                    var strModel = JSON.stringify(model);
-//                    console.log('strModel', strModel);
-                    var root = loader.loadModelFromString(strModel);
-                    console.log('root', root);
-                    var serializer = new Kevoree.org.kevoree.serializer.JSONModelSerializer();
-                    var os = new Kevoree.java.io.OutputStream();
-                    serializer.serialize(root, os);
-                    console.log('output stream', os);
+//                    var loader = new Kevoree.org.kevoree.loader.JSONModelLoader();
+//                    var strModel = JSON.stringify(model);
+////                    console.log('strModel', strModel);
+//                    var root = loader.loadModelFromString(strModel);
+//                    console.log('root', root);
+//                    var serializer = new Kevoree.org.kevoree.serializer.JSONModelSerializer();
+//                    var os = new Kevoree.java.io.OutputStream();
+//                    serializer.serialize(root, os);
+//                    console.log('output stream', os);
                     editor.setModel(model);
 
                     // update headsup for user
                     AlertPopupHelper.setText("Core library ("+lib+") loaded successfully");
                     AlertPopupHelper.show(5000);
-//                } catch (err) {
-//                    // update headsup for user
-//                    AlertPopupHelper.setText("Unable to load library ("+lib+")");
-//                    AlertPopupHelper.setType(AlertPopupHelper.ERROR);
-//                    AlertPopupHelper.show(5000);
-//                    console.error("Unable to load library ("+lib+")", (err.message) ? err.message : err);
-//                }
+                } catch (err) {
+                    // update headsup for user
+                    AlertPopupHelper.setText("Unable to load library ("+lib+")");
+                    AlertPopupHelper.setType(AlertPopupHelper.ERROR);
+                    AlertPopupHelper.show(5000);
+                    console.error("Unable to load library ("+lib+")", (err.message) ? err.message : err);
+                }
             });
         }
 
