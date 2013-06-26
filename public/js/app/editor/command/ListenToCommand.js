d949187292f95620d3e5a32029cdec4ede808fde
Maxime Tricoire <leiko@braindead.fr>
2013-06-26 16:19:20 +0200

    ListenTo upgrade

--- public/js/app/editor/command/ListenToCommand.js
+++ public/js/app/editor/command/ListenToCommand.js
@@ -22,6 +22,7 @@ define(
 
             ws.onopen = function () {
                 // you are connected
+                connectionSucceed(ws, uri);
             }
 
             ws.onclose = function () {
@@ -40,36 +41,53 @@ define(
             });
         }
 
+        function connectionSucceed(ws, uri) {
+            AlertPopupHelper.setHTML("Listening to "+uri+"...");
+            AlertPopupHelper.setType(AlertPopupHelper.SUCCESS);
+            AlertPopupHelper.show(3500);
+
+            successPopupUI(uri, function () {
+                ws.close();
+            });
+        }
+
         // private method
         function loadSucceed(ws, uri) {
             AlertPopupHelper.setText("New model received from ws://"+uri);
             AlertPopupHelper.setType(AlertPopupHelper.SUCCESS);
-            AlertPopupHelper.show(5000);
+            AlertPopupHelper.show(3500);
 
-            $('#listen-to-content').html(['Currently listening to <span class="text-info">', uri, '</span>'].join(''));
-            $('#listen-to-close').show();
-            $('#listen-to-close').off('click');
-            $('#listen-to-close').on('click', function () {
+
+            successPopupUI(uri, function () {
                 ws.close();
             });
-            $('#listen-to').hide();
         }
 
         function loadFailed(uri) {
             AlertPopupHelper.setHTML("Unable to connect to "+uri+".<br/> Aborting listening process...");
             AlertPopupHelper.setType(AlertPopupHelper.ERROR);
-            AlertPopupHelper.show(5000);
+            AlertPopupHelper.show(3500);
 
-            $('#listen-to-content').empty();
-            $('#listen-to-close').hide();
-            $('#listen-to').show();
+            errorPopupUI();
         }
 
         function connectionClosed() {
             AlertPopupHelper.setHTML('\"Listen to\" aborted. <br/> Connection closed.');
             AlertPopupHelper.setType(AlertPopupHelper.WARN);
-            AlertPopupHelper.show(5000);
+            AlertPopupHelper.show(3500);
 
+            errorPopupUI();
+        }
+
+        function successPopupUI(uri, closeCallback) {
+            $('#listen-to-content').html('Currently listening to <span class="text-info">' + uri + '</span>');
+            $('#listen-to-close').show();
+            $('#listen-to-close').off('click');
+            $('#listen-to-close').on('click', closeCallback);
+            $('#listen-to').hide();
+        }
+
+        function errorPopupUI() {
             $('#listen-to-content').empty();
             $('#listen-to-close').hide();
             $('#listen-to').show();
