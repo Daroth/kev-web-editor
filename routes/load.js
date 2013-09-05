673f34e2a04ab7861240913b64a56d08d16bb85c
Maxime Tricoire <leiko@braindead.fr>
2013-09-05 15:16:23 +0200

    using a cache for core libraries with an automatic cache clearing every 15 minutes

--- routes/load.js
+++ routes/load.js
@@ -10,61 +10,72 @@ var PLATFORMS = ['javase', 'android', 'sky'];
 
 var http = require('http');
 var parseString = require('xml2js').parseString;
+var libraries = {};
 
 exports.load = function (req, res) {
     if (req.xhr) {
         var platform = req.query.platform;
         if (platform) {
             if (PLATFORMS.indexOf(platform) != -1) {
-                var options = {
-                    host: 'oss.sonatype.org',
-                    path: '/service/local/data_index?g=org.kevoree.corelibrary.'+platform
-                }
-                var request = http.request(options, function (result) {
-                    var data = '';
-                    result.on('data', function (chunk) {
-                        data += chunk;
+                if (libraries[platform] != undefined && libraries[platform].length > 0) {
+                    // libraries have already been retrieved from oss.sonatype.org
+                    console.log('Sending cached libraries for '+platform);
+                    res.json({
+                        result: 1,
+                        message: 'Ok',
+                        libraries: libraries[platform]
                     });
-                    result.on('end', function () {
-                        // XML data fully retrieved from remote server => parsing and sending result
-                        parseString(data, function (err, xml) {
-                            var artifacts = xml['search-results']['data'][0]['artifact'];
-                            var libraries = [];
-                            for (var i=0; i < artifacts.length; i++) {
-                                var rawArt = artifacts[i]['artifactId'][0].split('.');
+                } else {
+                    var options = {
+                        host: 'oss.sonatype.org',
+                        path: '/service/local/data_index?g=org.kevoree.corelibrary.'+platform
+                    }
+                    var request = http.request(options, function (result) {
+                        var data = '';
+                        result.on('data', function (chunk) {
+                            data += chunk;
+                        });
+                        result.on('end', function () {
+                            // XML data fully retrieved from remote server => parsing and sending result
+                            libraries[platform] = []; // resetting library cache for this platform
+                            parseString(data, function (err, xml) {
+                                var artifacts = xml['search-results']['data'][0]['artifact'];
+                                for (var i=0; i < artifacts.length; i++) {
+                                    var rawArt = artifacts[i]['artifactId'][0].split('.');
 
-                                var grpId   = artifacts[i]['groupId'][0],
-                                    artId   = artifacts[i]['artifactId'][0],
-                                    name    = rawArt[rawArt.length-1],
-                                    version = artifacts[i]['version'][0];
+                                    var grpId   = artifacts[i]['groupId'][0],
+                                        artId   = artifacts[i]['artifactId'][0],
+                                        name    = rawArt[rawArt.length-1],
+                                        version = artifacts[i]['version'][0];
 
-                                if (artifacts[i]['classifier'] == undefined) { // XXX Warning, this could be really bad
-                                    libraries.push({
-                                        groupID:        grpId,
-                                        artifactID:     artId,
-                                        simpleName:     name,
-                                        version:        version
-                                    });
+                                    if (artifacts[i]['classifier'] == undefined) { // XXX Warning, this could be really bad
+                                        libraries[platform].push({
+                                            groupID:        grpId,
+                                            artifactID:     artId,
+                                            simpleName:     name,
+                                            version:        version
+                                        });
+                                    }
                                 }
-                            }
-                            res.json({
-                                result: 1,
-                                message: 'Ok',
-                                libraries: libraries
+                                res.json({
+                                    result: 1,
+                                    message: 'Ok',
+                                    libraries: libraries[platform]
+                                });
                             });
                         });
                     });
-                });
-                request.on('error', function (e) {
-                    // TODO handle unreachable event on oss.sonatype and try to get the libs from a local cache
-                    // if a connection to sonatype as already been made
-                    console.log('Unable to reach '+options.host+': ' + e.message);
-                    res.json({
-                        result: -1,
-                        message: 'Unable to reach '+options.host
+                    request.on('error', function (e) {
+                        // unable to reach oss.sonatype.org maven repository
+                        console.log('Unable to reach '+options.host+': ' + e.message);
+                        // display error
+                        res.json({
+                            result: -1,
+                            message: 'Unable to reach '+options.host
+                        });
                     });
-                });
-                request.end();
+                    request.end();
+                }
 
             } else {
                 res.json({
@@ -87,3 +98,11 @@ exports.load = function (req, res) {
         });
     }
 };
+
+// repetitive task to clear libraries cache every X ms
+(function () {
+    setInterval(function () {
+        console.log("Automatically cleared core libraries cache.");
+        libraries = {}; // clear cache
+    }, 1000*15); // do this every 15 minutes
+})();
