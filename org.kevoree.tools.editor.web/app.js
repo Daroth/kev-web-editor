2e1ec17cf956381cedd89c763954f6843ea4c949
Maxime Tricoire <leiko@braindead.fr>
2013-10-01 10:18:17 +0200

    fix route prefix in app.js

--- org.kevoree.tools.editor.web/app.js
+++ org.kevoree.tools.editor.web/app.js
@@ -14,7 +14,7 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || config.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(config.ROUTE_PREFIX, express.static(path.join(__dirname, 'public')));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -23,7 +23,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('M7q,$wfz|UyloWSQy[2mTl0<X4iU}++x}]nW6ef)>lO7os,:wKZ0g>?f0YG)U0FQ'));
 app.use(express.session());
-app.use(app.router);
+app.use(config.ROUTE_PREFIX, app.router);
 
 // development only
 app.configure('development', function() {
@@ -43,21 +43,21 @@ fs.readdir('public/saved/', function(err, files) {
     }
 });
 
-app.get(config.ROUTE_PREFIX+'/', routes.index);
-app.get(config.ROUTE_PREFIX+'/load', routes.load);
-app.get(config.ROUTE_PREFIX+'/merge', routes.merge);
-app.post(config.ROUTE_PREFIX+'/push', routes.push);
-app.post(config.ROUTE_PREFIX+'/pull', routes.pull);
-app.post(config.ROUTE_PREFIX+'/open', routes.open);
-app.post(config.ROUTE_PREFIX+'/save/:type', routes.save);
-app.get(config.ROUTE_PREFIX+'/saved/:type/:id', routes.saved);
-app.get(config.ROUTE_PREFIX+'/runtime', routes.runtime);
-app.get(config.ROUTE_PREFIX+'/bench', routes.bench);
+app.get('/', routes.index);
+app.get('/load', routes.load);
+app.get('/merge', routes.merge);
+app.post('/push', routes.push);
+app.post('/pull', routes.pull);
+app.post('/open', routes.open);
+app.post('/save/:type', routes.save);
+app.get('/saved/:type/:id', routes.saved);
+app.get('/runtime', routes.runtime);
+app.get('/bench', routes.bench);
 
 app.use(function(req, res) {
     // if you end-up here, it means that I do not know the given url
     // so for now => redirect to '/' but you can put a custom 404 if you want
-    res.redirect(config.ROUTE_PREFIX+'/');
+    res.redirect('/');
 });
 
 http.createServer(app).listen(app.get('port'), function() {
