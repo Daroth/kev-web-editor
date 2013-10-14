ef0a38845b735170cdd65761b00708acb0fb1dc2
Maxime Tricoire <max.tricoire@gmail.com>
2013-10-14 18:11:55 +0200

    handle push & pull for Javascript platform

--- org.kevoree.tools.editor.web/routes/pull.js
+++ org.kevoree.tools.editor.web/routes/pull.js
@@ -4,55 +4,76 @@
 
 var java = require('java');
 var config = require('../config');
+var pullModel = require('kevoree-model-sync').pullModel;
+var kevoree = require('kevoree-library').org.kevoree;
 java.classpath.push(config.KEV_JAR);
 // node-java module uses the 'Sync' syntax for methods executed directly, otherwise
 // you need to use callbacks to handle results (this has nothing to do with ModelSync sync keyword)
-var modelSync   = java.newInstanceSync('org.kevoree.tools.modelsync.ModelSyncBean'),
-    loader      = java.newInstanceSync('org.kevoree.loader.JSONModelLoader'),
-    serializer  = java.newInstanceSync('org.kevoree.serializer.JSONModelSerializer');
+var modelSync  = java.newInstanceSync('org.kevoree.tools.modelsync.ModelSyncBean'),
+  loader       = java.newInstanceSync('org.kevoree.loader.JSONModelLoader'),
+  jsLoader     = new kevoree.loader.JSONModelLoader(),
+  serializer   = java.newInstanceSync('org.kevoree.serializer.JSONModelSerializer'),
+  jsSerializer = new kevoree.serializer.JSONModelSerializer();
 
 exports.pull = function(req, res) {
-    if (req.xhr) {
-        if (req.body.destNodeName && req.body.grpName && req.body.model) {
-            var data = JSON.stringify(req.body.model, null, 4),
-                model = loader.loadModelFromStringSync(data).getSync(0);
+  if (req.xhr) {
+    if (req.body.destNodeName && req.body.grpName && req.body.model) {
+      var data    = JSON.stringify(req.body.model, null, 4),
+          model   = loader.loadModelFromStringSync(data).getSync(0),
+          jsModel = jsLoader.loadModelFromString(data).get(0);
 
-            modelSync.clearSync();
-            modelSync.pullTo(model, req.body.destNodeName, req.body.grpName, function (err, model) {
-                if (err) {
-                    console.error(err);
-                    res.json({result: -1, message: 'Server-side java exception'});
-                    return;
-                }
+      var kGrp = jsModel.findGroupsByID(req.body.grpName);
+      if (kGrp.typeDefinition.deployUnits.get(0).targetNodeType.name == 'JavascriptNode') {
+        // Javascript platform pull
+        pullModel(jsModel, req.body.destNodeName, function (err, model) {
+          if (err) return res.json({result: -1, message: 'Unable to pull model from '+req.body.destNodeName});
 
-                java.newInstance('java.io.ByteArrayOutputStream', function (err, baos) {
-                    if (err) {
-                        console.error(err);
-                        res.json({result: -1, message: 'Server-side java exception'});
-                        return;
-                    }
+          return res.json({
+            result: 1,
+            message: 'Model pulled successfully',
+            model: jsSerializer.serialize(model)
+          });
+        });
 
-                    serializer.serialize(model, baos, function (err) {
-                        if (err) {
-                            console.error(err);
-                            res.json({result: -1, message: 'Server-side java exception'});
-                            return;
-                        }
-
-                        res.json({
-                            result: 1,
-                            message: 'Model pulled successfully',
-                            model: JSON.parse(baos.toStringSync())
-                        });
-                        return;
-                    });
-                });
-            });
-        } else {
-            res.json({result: -1, message: 'Missing parameters in request'});
+      } else {
+        // Java platform pull
+        modelSync.clearSync();
+        modelSync.pullTo(model, req.body.destNodeName, req.body.grpName, function (err, model) {
+          if (err) {
+            console.error(err);
+            res.json({result: -1, message: 'Server-side java exception'});
             return;
-        }
+          }
+
+          java.newInstance('java.io.ByteArrayOutputStream', function (err, baos) {
+            if (err) {
+              console.error(err);
+              res.json({result: -1, message: 'Server-side java exception'});
+              return;
+            }
+
+            serializer.serialize(model, baos, function (err) {
+              if (err) {
+                console.error(err);
+                res.json({result: -1, message: 'Server-side java exception'});
+                return;
+              }
+
+              res.json({
+                result: 1,
+                message: 'Model pulled successfully',
+                model: JSON.parse(baos.toStringSync())
+              });
+              return;
+            });
+          });
+        });
+      }
     } else {
-        res.json({result: -1, message: 'Not an Ajax call'});
+      res.json({result: -1, message: 'Missing parameters in request'});
+      return;
     }
+  } else {
+    res.json({result: -1, message: 'Not an Ajax call'});
+  }
 };
--- org.kevoree.tools.editor.web/routes/push.js
+++ org.kevoree.tools.editor.web/routes/push.js
@@ -1,26 +1,35 @@
 /*
  * POST push model
  */
-
 var java = require('java');
 var config = require('../config');
+var pushModel = require('kevoree-model-sync').pushModel;
+var kevoree = require('kevoree-library').org.kevoree;
+
 java.classpath.push(config.KEV_JAR);
 // node-java module uses the 'Sync' syntax for methods executed directly, otherwise
 // you need to use callbacks to handle results (this has nothing to do with ModelSync sync keyword)
 var modelSync   = java.newInstanceSync('org.kevoree.tools.modelsync.ModelSyncBean'),
-  loader      = java.newInstanceSync('org.kevoree.loader.JSONModelLoader');
+    loader      = java.newInstanceSync('org.kevoree.loader.JSONModelLoader'),
+    jsLoader    = new kevoree.loader.JSONModelLoader();
 
 exports.push = function(req, res) {
   if (req.xhr) {
     if (req.body.destNodeName && req.body.grpName && req.body.model) {
-      var data = JSON.stringify(req.body.model, null, 4),
-        model = loader.loadModelFromStringSync(data).getSync(0);
+      var data    = JSON.stringify(req.body.model, null, 4),
+          model   = loader.loadModelFromStringSync(data).getSync(0),
+          jsModel = jsLoader.loadModelFromString(data).get(0);
 
-      var kGrp = model.findGroupsByIDSync(req.body.grpName);
-      if (kGrp.getTypeDefinitionSync().getDeployUnitsSync().getSync(0).getTargetNodeTypeSync().getNameSync() == 'JavascriptNode') {
-        console.log("PUSH on groups running on JavascriptNode are not yet handled.");
-        res.json({result: -1, message: 'Push request for JavascriptNode platform is not handled yet.'});
+      var kGrp = jsModel.findGroupsByID(req.body.grpName);
+      if (kGrp.typeDefinition.deployUnits.get(0).targetNodeType.name == 'JavascriptNode') {
+        // Javascript platform push
+        pushModel(jsModel, req.body.destNodeName, function (err) {
+          if (err) return res.json({result: -1, message: 'Unable to push model to group '+req.body.destNodeName});
+
+          return res.json({result: 1, message: 'Model pushed successfully'});
+        });
       } else {
+        // Java platform push
         modelSync.clearSync();
         modelSync.pushTo(model, req.body.destNodeName, req.body.grpName, function (err) {
           if (err) {
